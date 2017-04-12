cfcf127dbb29b74db8c0bae549721ba1c52dae59
Mégane Vilain <megane.vilain@gmail.com>
2017-04-12 13:32:59 +0200

    Implémentation du Navigation Drawer

--- Android-Maths/app/src/main/java/com/startup/jinx/android_maths/HomeActivity.java
+++ Android-Maths/app/src/main/java/com/startup/jinx/android_maths/HomeActivity.java
@@ -1,13 +1,60 @@
 package com.startup.jinx.android_maths;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    // Navigation menu
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle mActionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open,R.string.nav_drawer_close) {
+            @Override
+            public void onDrawerOpened (View view){
+                super.onDrawerOpened(view);
+            }
+
+            @Override
+            public void onDrawerClosed(View view){
+                super.onDrawerClosed(view);
+            }
+        };
+        assert drawerLayout != null;
+        drawerLayout.addDrawerListener(mActionBarDrawerToggle);
+        mActionBarDrawerToggle.syncState();
+
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        assert mNavigationView != null;
+        mNavigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+        int id = item.getItemId();
+
+        Intent intent;
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        assert drawer != null;
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
