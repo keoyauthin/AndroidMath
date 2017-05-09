359413b253f0a9cc5e884ae590975154b2b7a304
Megane Vilain <Megane Vilain>
2017-05-09 21:17:38 +0200

    Correction de bugs divers

--- Android-Maths/app/src/main/java/com/startup/jinx/android_maths/fragment/Polynome_Second_Ordre_Fragment.java
+++ Android-Maths/app/src/main/java/com/startup/jinx/android_maths/fragment/Polynome_Second_Ordre_Fragment.java
@@ -2,10 +2,13 @@ package com.startup.jinx.android_maths.fragment;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -93,7 +96,7 @@ public class Polynome_Second_Ordre_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
     {
-        View view = inflater.inflate(R.layout.polynome_second_ordre_fragment,container,false);
+        final View view = inflater.inflate(R.layout.polynome_second_ordre_fragment,container,false);
 
         spinner = (Spinner) view.findViewById(R.id.polynome_activity_spinner);
         spinner2 = (Spinner) view.findViewById(R.id.polynome_activity_spinner2);
@@ -105,6 +108,8 @@ public class Polynome_Second_Ordre_Fragment extends Fragment {
         Edit_c = (EditText) view.findViewById(R.id.polynome_second_ordre_c);
 
         resulat_X1 = (TextView) view.findViewById(R.id.polynome_second_ordre_resultat_X1);
+        resultat_X2 = (TextView) view.findViewById(R.id.polynome_second_ordre_resultat_X2);
+        resultat_X2 = (TextView) view.findViewById(R.id.polynome_second_ordre_resultat_X2);
         delta_ope = (TextView) view.findViewById(R.id.polynome_second_ordre_delta_ope);
         delta_resultat = (TextView) view.findViewById(R.id.polynome_second_ordre_delta_resultat);
         factorisation_resultat = (TextView) view.findViewById(R.id.polynome_second_ordre_factorisation_resultat);
@@ -121,6 +126,14 @@ public class Polynome_Second_Ordre_Fragment extends Fragment {
         spinner.setAdapter(adapter);
         spinner2.setAdapter(adapter);
 
+        Edit_c.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                Math_Utils.hideKeyboard(getContext(),view);
+                Check_Values();
+                return true;
+            }
+        });
 
         Calculbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -143,19 +156,21 @@ public class Polynome_Second_Ordre_Fragment extends Fragment {
      */
     public void Check_Values()
     {
-        double a,b,c;
+        double a =0;
+        double b =0;
+        double c =0;
 
-        if(Edit_a.getText().toString().isEmpty())
-        {
-            a = 0;
-        }else if (Edit_b.getText().toString().isEmpty()){
-            b =0;
-        }else if (Edit_c.getText().toString().isEmpty()){
-            c=0;
+        if (!Edit_a.getText().toString().isEmpty()) {
+            a = Double.parseDouble(Edit_a.getText().toString());
         }
-        a = Double.parseDouble(Edit_a.getText().toString());
-        b = Double.parseDouble(Edit_b.getText().toString());
-        c= Double.parseDouble(Edit_c.getText().toString());
+        if (!Edit_b.getText().toString().isEmpty()) {
+            b = Double.parseDouble(Edit_b.getText().toString());
+        }
+
+        if (!Edit_c.getText().toString().isEmpty()) {
+            c = Double.parseDouble(Edit_c.getText().toString());
+        }
+
 
         if(spinner.getSelectedItem().toString().equals("-")){
             b= b*-1;
@@ -191,34 +206,26 @@ public class Polynome_Second_Ordre_Fragment extends Fragment {
             linearLayoutDelta.setVisibility(View.VISIBLE);
 
             delta_ope.setText("Delta = " + String.valueOf(b) +"² +  4x" + String.valueOf(a) + "x" +String.valueOf(c) );
+            //delta_ope.setText(getString(R.string.polynome_activity_delta_delta_ope), b, a ,c);
 
             delta_resultat.setVisibility(View.VISIBLE);
-            delta_resultat.setText("Delta = " + String.valueOf(delta));
-
+            delta_resultat.setText("Delta = " + String.valueOf(Math_Functions.Round_Doucle(delta,3)));
             if(delta == 0){
                 X1 = ((b*-1)+Math.sqrt(delta)) / (2*a);
                 resulat_X1.setText("X1= " + String.valueOf(Math_Functions.Round_Doucle(X1,3)));
-                factorisation_resultat.setText("P(X) = " + String.valueOf(a) + "( x - " + String.valueOf(X1) + " )");
+                factorisation_resultat.setText("P(X) = " + String.valueOf(a) + "( x - " + String.valueOf(Math_Functions.Round_Doucle(X1,3)) + " )");
             }
             else if (delta > 0){
                 X1 = ((b*-1) + Math.sqrt(delta)) / (2*a);
                 X2 = ((b*-1) - Math.sqrt(delta)) / (2*a);
-                resulat_X1.setText("X1 " + String.valueOf(Math_Functions.Round_Doucle(X1,3)));
-                resultat_X2.setText(" X2= " + String.valueOf(Math_Functions.Round_Doucle(X2,3)));
-                factorisation_resultat.setText("P(X) = " + String.valueOf(a) + "( x - " + String.valueOf(X1) + " )( x - " + String.valueOf(X2) + ")" );
+                resulat_X1.setText("X1= " + String.valueOf(Math_Functions.Round_Doucle(X1,3)));
+                resultat_X2.setText("X2= " + String.valueOf(Math_Functions.Round_Doucle(X2,3)));
+                factorisation_resultat.setText("P(X) = " + String.valueOf(a) + "( x - (" + String.valueOf(Math_Functions.Round_Doucle(X1,3)) + " ))( x - (" + String.valueOf(Math_Functions.Round_Doucle(X2,3)) + "))" );
             }
             else if (delta <0){
                 ////Pas de solutions possibles
-                resulat_X1.setText("Il n'existe pas de solutions possibles pour cette équation");
-                /*X1 = 2;
-                X2 = 1;
-                resulat_X1.setText();*/
+                resulat_X1.setText(R.string.polynome_activity_zero_solution);
             }
         }
-
-
-
-
     }
-
 }
--- Android-Maths/app/src/main/java/com/startup/jinx/android_maths/fragment/Proba_Loi_Binomiale_Fragment.java
+++ Android-Maths/app/src/main/java/com/startup/jinx/android_maths/fragment/Proba_Loi_Binomiale_Fragment.java
@@ -1,6 +1,13 @@
 package com.startup.jinx.android_maths.fragment;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.startup.jinx.android_maths.R;
 
 /**
  * Created by Mégane Vilain on 18/04/2017.
@@ -8,4 +15,33 @@ import android.support.v4.app.Fragment;
  */
 
 public class Proba_Loi_Binomiale_Fragment extends Fragment {
+    /**
+     * Instanticie le fragment.
+     */
+    public Proba_Loi_Binomiale_Fragment(){
+        //Required empty public constructor
+    }
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.proba_loi_binomiale_fragment,container,false);
+
+
+        return view;
+    }
+
+    @Override
+    public void onDetach(){
+        super.onDetach();
+    }
+
 }
